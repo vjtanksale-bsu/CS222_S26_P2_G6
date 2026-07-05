692506eaa91bad8da7d0b5cad376b3e683f7c085
MatthewY128 <jsmhgqrxt7@privaterelay.appleid.com>
2026-07-05 03:18:45 -0400

    add failing test

---
+++ test_course_data.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+
+from course_data import read_course_data
+
+
+class TestCourseData(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = "test_courses.txt"
+
+        with open(self.filename, "w") as file:
+            file.write("CS120 001 MWF 0900 0950\n")
+            file.write("CS120 002 TR 1100 1215\n")
+            file.write("CS121 001 MWF 1000 1050\n")
+
+    def tearDown(self):
+        os.remove(self.filename)
+
+    def test_read_course_data(self):
+        courses = read_course_data(self.filename)
+
+        self.assertEqual(len(courses), 3)
