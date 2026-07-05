f009f36ac864b393c65782180ce08908b8c0cfa8
MatthewY128 <jsmhgqrxt7@privaterelay.appleid.com>
2026-07-05 03:31:59 -0400

    add failing test for course number and display

--- test_course_data.py
+++ test_course_data.py
@@ -1,7 +1,13 @@
 import unittest
 import os
+import sys
+from io import StringIO
 
-from course_data import read_course_data
+from course_data import (
+    read_course_data,
+    get_course_numbers,
+    display_course_numbers
+)
 
 
 class TestCourseData(unittest.TestCase):
@@ -26,3 +32,27 @@ class TestCourseData(unittest.TestCase):
         self.assertEqual(courses[0]["days"], "MWF")
         self.assertEqual(courses[0]["start"], 900)
         self.assertEqual(courses[0]["end"], 950)
+
+    def test_get_course_numbers(self):
+        courses = read_course_data(self.filename)
+
+        self.assertEqual(
+            get_course_numbers(courses),
+            ["CS120", "CS121"]
+        )
+
+    def test_display_course_numbers(self):
+        courses = read_course_data(self.filename)
+
+        captured = StringIO()
+        sys.stdout = captured
+
+        display_course_numbers(courses)
+
+        sys.stdout = sys.__stdout__
+
+        output = captured.getvalue()
+
+        self.assertIn("Available Courses", output)
+        self.assertIn("CS120", output)
+        self.assertIn("CS121", output)
